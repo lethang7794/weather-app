acfc6e58534fbd0577dff5c0c9e87a46317bf887
Thang Le Quang <lethang7794@gmail.com>
2021-03-20 17:06:12 +0700

    Replace Github navlink with Github Corner

--- src/components/SiteNavBar.js
+++ src/components/SiteNavBar.js
@@ -1,16 +1,8 @@
 import { Navbar, Nav } from 'react-bootstrap';
 
 const SiteNavBar = () => (
-  <Navbar bg='primary' variant='dark' expand='lg'>
-    <Navbar.Brand href='/'>THANG'S WEATHER APP</Navbar.Brand>
-    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-    <Navbar.Collapse id='basic-navbar-nav'>
-      <Nav className='mr-auto'>
-        <Nav.Link href='https://github.com/lethang7794/weather-app'>
-          Github
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar fixed='top' expand='lg'>
+    <Navbar.Brand href='/'>WEATHER APP</Navbar.Brand>
   </Navbar>
 );
 
