a1e879b6af4afcf55a0071b8f3f04f5c225ca2bb
Thang Le Quang <lethang7794@gmail.com>
2021-03-20 15:52:28 +0700

    Update Github repo link

--- src/components/SiteNavBar.js
+++ src/components/SiteNavBar.js
@@ -1,12 +1,14 @@
 import { Navbar, Nav } from 'react-bootstrap';
 
 const SiteNavBar = () => (
-  <Navbar bg="primary" variant="dark" expand="lg">
-    <Navbar.Brand href="#home">THANG'S WEATHER APP</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#">Github</Nav.Link>
+  <Navbar bg='primary' variant='dark' expand='lg'>
+    <Navbar.Brand href='/'>THANG'S WEATHER APP</Navbar.Brand>
+    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+    <Navbar.Collapse id='basic-navbar-nav'>
+      <Nav className='mr-auto'>
+        <Nav.Link href='https://github.com/lethang7794/weather-app'>
+          Github
+        </Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
